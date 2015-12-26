2e23e641ce41f0a30fd427814c6fedd29df82ada
Chuck <chuck@moosejudge.com>
2015-12-26 15:04:25 -0800

    regular commit

---
+++ files/sysowner_patch_install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#
+# future script to install patches based on sysowner module cron configuration
+#
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,25 @@
 # Class: sysowner
 #
-# This module manages sysowner
+# Manage system owners and roles with fact files on system
 #
-# Parameters: none
+# Parameters:
+#
+# System parameters:
+# system_owners: array of system owner names (should be valid accounts on system)
+# system_groups: array of system groups (group on server not required)
+# system_note: short description of server usage
+#
+# Support parameters:
+# support_team: name of OS/Hardware team supporting the system
+# support_contact: email contact of support team
+# support_level: level of support for system defaults(no-page, work-day, backup-only, 24x7)
+#
+# Module behavior parameters:
+# fact_template: ERB template to use for writing facts
+# fact_file: file location of custom facts
+# python_install: true or false: install python and PyYAML
+# python_pkg: name of python (rpm/yum) package for install
+# python_yaml_pky: name of python module (rpm/yum) package for install
 #
 # Actions:
 #
@@ -11,18 +28,40 @@
 # Sample Usage:
 #
 class sysowner (
-  $enable           = true, # true | false | absent
+  # Basic system info
   $system_owners    = [], # array of owners
   $system_groups    = [], # array of groups
   $system_note      = 'General Purpose Server', # free text to describe the server
-  $support_level    = 'no-page', # free text to define support level
-  # work-day | 24x7 | backup-only | no-page
+  # Support information for system
+  $support_team     = 'UCAT', # free text of support team name
+  $support_level    = 'no-page', # work-day | 24x7 | backup-only | no-page
   $support_contact  = 'ucat@ohsu.edu', # support contact email
-  $fact_template    = "templates/system_owner.erb",
+  $support_qpage    = 'unix_oncall', # support qpage contact
+  # Patch details for system
+  # TODO: patch control will come in v2 - ignore for now
+  # TODO: too much choice for patch install - maybe: monthly, quarterly, yearly
+  $patch_control    = false, # should sysowner manage patch installs
+  $patch_reboot     = true, # should system reboot after patch install
+  $patch_hour       = '3', # hour of the day for patch install
+  $patch_minute     = '0', # minute of the hour for patch install
+  $patch_weekday    = '1', # day of week for patch install
+  $patch_month      = '3,6,9,13', # month of the year for patch install
+  $patch_monthday   = '15', # day of month for patch install
+  $patch_script_src = 'files/sysowner_patch_install.sh', # cron script source for patch install
+  $patch_script_dst = '/usr/local/bin/sysowner_patch_install.sh', # cron script destination for patch install
+  # module behavior control
+  $fact_template    = "templates/system_owner.erb", # template for fact file
+  $fact_file        = "/etc/facter/facts.d/system_owner.yaml", # location of fact file on system
+  $python_install   = true, # should the module install python
+  $python_pkg       = "python", # name of package with python
+  $python_yaml_pkg  = "PyYAML", # name of package with python PyYAML module
 )
 {
-  if ! is_bool($enable) { fail("enable must be true/false") }
-  if ! is_array($owners) and ! empty($owners) { fail("owners not a valid array")}
-  if ! is_array($groups) and ! empty($groups) { fail("groups not a valid array")}  
+  # variable verification
+  if ! is_array($system_owners) and ! empty($system_owners) { fail("system owners not a valid array.")}
+  if ! is_array($system_groups) and ! empty($system_groups) { fail("system groups not a valid array.")}
+  if ! is_string($system_note) { fail("system note not a valid string.")}
+
+
   
 }
