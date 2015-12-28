b43e38271c76cedeecd9fbd341db1b34e0121917
Chuck <chuck@moosejudge.com>
2015-12-27 19:03:06 -0800

    template format

--- README.md
+++ README.md
@@ -1,28 +1,70 @@
 # Class: sysowner #
 
- Manage system owners and support information with facts on system
+ Manage system owners and groups, system role, and patch updating. 
 
-# Parameters: #
+## Parameters: 
 
-## System parameters:
-- system_owners: array of system owner names (should be valid accounts on system)
-- system_groups: array of system groups (group on server not required)
-- system_note: short description of server usage
+### Basic system info
+  - system_owners: free form array of "system owners", could be emails, local account, names
+  - system_groups: free form array of "system owner groups", AD groups, local unix groups, team names
+  - system_role: free form string. ex: general | web | mysql | oracle | etl | epic
+    - can be used in hiera. ex: "roles/%{::sysowner:system_role}" => roles/web.yaml
+  - system_note: free form string to describe the server
+  
+```
+sysowner::system_owners: 
+  - Henry Winkler
+sysowner::system_groups:
+   - AD HW Fan
+sysowner::system_role: 'general'
+sysowner::system_note: 'General Purpose Server'
+```
 
-## Support parameters:
-- support_team: name of OS/Hardware team supporting the system
-- support_contact: email contact of support team
-- support_level: level of support for system defaults(no-page, work-day, backup-only, 24x7)
+### Support information for system
+  - support_team: free form string for support team name
+  - support_level: free form string of support level
+  - support_contact: support contact email
 
-## Module behavior parameters:
-- fact_template: ERB template to use for writing facts
-- fact_file: file location of custom facts
-- python_install: true or false: install python and PyYAML
-- python_pkg: name of python (rpm/yum) package for install
-- python_yaml_pky: name of python module (rpm/yum) package for install
+```
+sysowner::support_team: 'unix team'
+sysowner::support_level: 'no-page'
+sysowner::support_contact: 'root@localhost'
+```
 
-# Actions: #
+### Optional parameters (one-off custom configs)
+  - oracle_client: configure for oracle client install
+    - user in hiera ex: "oraclient_%{::sysowner:oracle_client}" => oraclient_true.yaml
+  
+```
+sysowner::oracle_client = false
+```
 
-# Requires: see Modulefile #
+### Patch methods - disable | cron | yum_cron
+#### disable 
+```
+sysowner::patch_method: 'disable'
+```
+#### cron
+```
+sysowner::patch_method:     'cron'
+sysowner::patch_reboot:     false, 
+sysowner::patch_minute:     '45'
+sysowner::patch_hour:       '3'
+sysowner::patch_monthday:   '15'
+sysowner::patch_month:      '*'
+sysowner::patch_weekday:    '1'
+sysowner::patch_script_src: 'puppet:///modules/sysowner/sysowner_patch_install.sh'
+sysowner::patch_script_dst: '/usr/local/bin/sysowner_patch_install.sh'
+```
+#### yum-cron
+```
+sysowner::patch_apply_updates:      false
+sysowner::patch_download_updates:   true
+sysowner::patch_days_of_week:       '12345'
+sysowner::patch_upgrade_cmd:        'minimal-security'
+```
 
-# Sample Usage: #
+## Requires
+
+  - [treydock/yum_cron](https://forge.puppetlabs.com/treydock/yum_cron)
+  - [puppetlabs/stdlib](https://forge.puppetlabs.com/puppetlabs/stdlib)
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,8 @@
 #
 class sysowner (
   # Basic system info
-  $system_owners    = [], # array of owners
-  $system_groups    = [], # array of groups
+  $system_owners    = ['Henry Winkler',], # array of owners
+  $system_groups    = ['AD HW Fans',], # array of groups
   $system_role      = 'general', # system role - general | web | mysql | oracle | etl | epic
     # used in hiera ex: "roles/%{::sysowner:system_role}" => roles/web.yaml
   $system_note      = 'General Purpose Server', # free text to describe the server
@@ -13,26 +13,38 @@ class sysowner (
   $support_team     = 'unix team', # free text of support team name
   $support_level    = 'no-page', # work-day | 24x7 | backup-only | no-page
   $support_contact  = 'root@localhost', # support contact email
-  # Optional parameter
+  # Optional parameters (one-off custom configs)
   $oracle_client = false, # configure for oracle client install
     # user in hiera ex: "oraclient_%{::sysowner:oracle_client}" => oraclient_true.yaml
   # module behavior control
   $fact_template    = "sysowner/system_owner_facts.erb", # ERB template for fact file
   $fact_file        = "/etc/facter/facts.d/system_owner_facts.yaml", # location of fact file on system
   # Patch details for system
-  $patch_method     = 'disable', # how to patch - yum-cron | cron
+  $patch_method     = 'disable', # how to patch - yum_cron | cron
     # disable: do nothing *default*
     # cron: runs script from cron
-    # yum-cron: uses
+    # yum_cron: uses treydock/yum-cron module to configure yum_cron
+  # cron variables
   $patch_reboot     = false, # should system reboot after patch install
-  $patch_minute     = '15', # minute of the hour for patch install
+  $patch_minute     = '45', # minute of the hour for patch install
   $patch_hour       = '3', # hour of the day for patch install
   $patch_monthday   = '15', # day of month for patch install
   $patch_month      = '*', # month of the year for patch install
   $patch_weekday    = '1', # day of week for patch install
   $patch_script_src = 'puppet:///modules/sysowner/sysowner_patch_install.sh', # cron script source for patch install
   $patch_script_dst = '/usr/local/bin/sysowner_patch_install.sh', # cron script destination for patch install
-)
+  # yum-cron variables
+  $patch_apply_updates    = false, # will apply updates after download
+  $patch_download_updates = true, # must me true if apply_updates true
+  $patch_days_of_week     = '12345', # only check on work-days
+  $patch_upgrade_cmd      = 'minimal-security', # only valid for =>EL7
+    # default                            = yum upgrade
+    # security                           = yum --security upgrade
+    # security-severity:Critical         = yum --sec-severity=Critical upgrade
+    # minimal                            = yum --bugfix upgrade-minimal
+    # minimal-security                   = yum --security upgrade-minimal
+    # minimal-security-severity:Critical =  --sec-severity=Critical upgrade-minimal
+  )
 {
 
   # verify system_owners and system_groups
@@ -43,7 +55,7 @@ class sysowner (
   validate_bool($oracle_client)
 
   # verify support patch method
-  validate_re($patch_method, [ '^disable', '^cron', '^yum-cron'])
+  validate_re($patch_method, [ '^disable', '^cron', '^yum_cron'])
 
   # fact file for system
   file { "$fact_file":
@@ -56,23 +68,30 @@ class sysowner (
 
   # define patch_method settings
   case $patch_method {
-    'yum-cron': {
+    'yum_cron': {
+      # settings for yum_cron
       $patch_script_ensure = 'absent'
       $cron_entry_ensure = 'absent'
       $yum_cron_ensure = 'present'
       $yum_cron_enable = true
-
-
-      include ::yum-cron
     }
     'cron':     {
+      # settings for cron
       $patch_script_ensure = 'file'
       $cron_entry_ensure = 'present'
       $yum_cron_ensure = 'absent'
       $yum_cron_enable = false
 
     }
+    'disable':    {
+      # settings for disable
+      $patch_script_ensure = 'absent'
+      $cron_entry_ensure = 'absent'
+      $yum_cron_ensure = 'absent'
+      $yum_cron_enable = false
+    }
     default:    {
+      # disable it all
       $patch_script_ensure = 'absent'
       $cron_entry_ensure = 'absent'
       $yum_cron_ensure = 'absent'
@@ -101,6 +120,13 @@ class sysowner (
     weekday => $patch_weekday,
   }
 
-  # configure yum-cron
-
+  # configure yum_cron
+  class { 'yum_cron':
+    ensure => $yum_cron_ensure,
+    enable => $yum_cron_enable,
+    apply_updates => $patch_apply_updates,
+    download_updates => $patch_download_updates,
+    days_of_week => $patch_days_of_week,
+    upgrade_cmd => $patch_upgrade_cmd,
+  }
 }
