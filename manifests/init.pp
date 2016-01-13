34f50b55cf53fbb32949d1bee869fec9d320dd3a
Chuck <chuck@moosejudge.com>
2016-01-12 22:41:37 -0800

    Lots of untested changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,40 +14,16 @@ class sysowner (
   $support_level    = 'no-page', # work-day | 24x7 | backup-only | no-page
   $support_contact  = 'root@localhost', # support contact email
   $support_pager    = 'unix-team', # support contact pager
+
   # Optional parameters (one-off custom configs)
-  $oracle_client = false, # configure for oracle client install
   # user in hiera ex: "oraclient_%{::sysowner:oracle_client}" => oraclient_true.yaml
-  # module behavior control
+  $oracle_client    = false, # configure for oracle client install
+
+  # Control of fact file
   $fact_template    = 'sysowner/system_owner_facts.erb', # ERB template for fact file
   $fact_file        = '/etc/facter/facts.d/system_owner_facts.yaml', # location of fact file on system
-  # Patch details for system
-  $patch_method     = 'disable', # how to patch - yum_cron | cron
-  # disable: do nothing *default*
-  # cron: runs script from cron
-  # yum_cron: uses treydock/yum-cron module to configure yum_cron
-  # cron variables
-  $patch_reboot     = false, # should system reboot after patch install
-  $patch_minute     = '45', # minute of the hour for patch install
-  $patch_hour       = '3', # hour of the day for patch install
-  $patch_monthday   = '15', # day of month for patch install
-  $patch_month      = '*', # month of the year for patch install
-  $patch_weekday    = '1', # day of week for patch install
-  $patch_script_src = 'puppet:///modules/sysowner/sysowner_patch_install.sh', # cron script source for patch install
-  $patch_script_dst = '/usr/local/bin/sysowner_patch_install.sh', # cron script destination for patch install
-  # yum-cron variables
-  $patch_apply_updates    = false, # will apply updates after download
-  $patch_download_updates = true, # must me true if apply_updates true
-  $patch_days_of_week     = '12345', # only check on work-days
-  $patch_update_cmd      = 'minimal-security', # only valid for =>EL7
-  # default                            = yum upgrade
-  # security                           = yum --security upgrade
-  # security-severity:Critical         = yum --sec-severity=Critical upgrade
-  # minimal                            = yum --bugfix upgrade-minimal
-  # minimal-security                   = yum --security upgrade-minimal
-  # minimal-security-severity:Critical =  --sec-severity=Critical upgrade-minimal
-)
-{
-
+  $fact_multi       = false, # sysowner1 or sysowner[1]
+) {
   # verify system_owners and system_groups
   validate_array($system_owners)
   validate_array($system_groups)
@@ -55,12 +31,17 @@ class sysowner (
   # verify oracle_client boolean
   validate_bool($oracle_client)
 
-  # verify support patch method
-  validate_re($patch_method, [ '^disable', '^cron', '^yum_cron'])
+  # include patch control
+  include sysowner::patch
 
-  # ensure facter directory exists
-  file { '/etc/facter': ensure => 'directory', replace => 'no', }
-  file { '/etc/facter/facts.d': ensure => 'directory', replace => 'no', }
+  # base directory of fact file locaiton
+  $basedir = dirname($fact_file)
+
+  # exec to create basedir
+  exec { 'make_basedir':
+    command => "/bin/mkdir -p ${basedir}",
+    unless  => "/bin/ls -d ${basedir}",
+  }
 
   # fact file for system
   file { $fact_file:
@@ -69,71 +50,6 @@ class sysowner (
     owner   => 'root',
     group   => 'puppet',
     mode    => '0644',
-  }
-
-  # define patch_method settings
-  case $patch_method {
-    'yum_cron': {
-      # settings for yum_cron
-      $patch_script_ensure = 'absent'
-      $cron_entry_ensure = 'absent'
-      $yum_cron_ensure = 'present'
-      $yum_cron_enable = true
-    }
-    'cron': {
-      # settings for cron
-      $patch_script_ensure = 'file'
-      $cron_entry_ensure = 'present'
-      $yum_cron_ensure = 'absent'
-      $yum_cron_enable = false
-
-    }
-    'disable': {
-      # settings for disable
-      $patch_script_ensure = 'absent'
-      $cron_entry_ensure = 'absent'
-      $yum_cron_ensure = 'absent'
-      $yum_cron_enable = false
-    }
-    default: {
-      # disable it all
-      $patch_script_ensure = 'absent'
-      $cron_entry_ensure = 'absent'
-      $yum_cron_ensure = 'absent'
-      $yum_cron_enable = false
-    }
-  }
-
-  # patch script configuration
-  file { $patch_script_dst:
-    ensure  => $patch_script_ensure,
-    source  => $patch_script_src,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-  }
-
-  # patch script cron entry
-  cron { 'syswoner-patch-install':
-    ensure    => $cron_entry_ensure,
-    command   => "${patch_script_dst} 2>&1 >/dev/null",
-    user      => 'root',
-    minute    => $patch_minute,
-    hour      => $patch_hour,
-    monthday  => $patch_monthday,
-    month     => $patch_month,
-    weekday   => $patch_weekday,
-  }
-
-  # configure yum_cron
-  class { 'yum_cron':
-    ensure => $yum_cron_ensure,
-    enable => $yum_cron_enable,
-    mailto => $support_contact,
-
-    apply_updates => $patch_apply_updates,
-    download_updates => $patch_download_updates,
-    days_of_week => $patch_days_of_week,
-    update_cmd => $patch_update_cmd,
+    require => Exec['make_basedir'],
   }
 }
---
+++ manifests/patch.pp
@@ -0,0 +1,102 @@
+class sysowner::patch (
+    # Patch details for system
+    $method     = 'disable', # how to patch - yum_cron | cron
+    # disable: do nothing *default*
+    # cron: runs script from cron
+    # yum_cron: uses treydock/yum-cron module to configure yum_cron
+
+    # cron variables
+    $cron_reboot     = 'false', # should system reboot after patch install
+    $cron_minute     = '45', # minute of the hour for patch install
+    $cron_hour       = '3', # hour of the day for patch install
+    $cron_monthday   = '15', # day of month for patch install
+    $cron_month      = '*', # month of the year for patch install
+    $cron_weekday    = '1', # day of week for patch install
+    $cron_script_src = 'puppet:///modules/sysowner/sysowner_patch_install.sh', # cron script source for patch install
+    $cron_script_dst = '/usr/local/bin/sysowner_patch_install.sh', # cron script destination for patch install
+    # yum-cron variables
+    $yum_cron_apply_updates    = false, # will apply updates after download
+    $yum_cron_download_updates = true, # must me true if apply_updates true
+    $yum_cron_days_of_week     = '12345', # only check on work-days
+    $yum_cron_update_cmd      = 'minimal-security', # only valid for =>EL7
+    $yum_cron_mailto            = $sysowner::support_contact,
+    # default                            = yum upgrade
+    # security                           = yum --security upgrade
+    # security-severity:Critical         = yum --sec-severity=Critical upgrade
+    # minimal                            = yum --bugfix upgrade-minimal
+    # minimal-security                   = yum --security upgrade-minimal
+    # minimal-security-severity:Critical =  --sec-severity=Critical upgrade-minimal
+) {
+
+    # verify support patch method
+    validate_re($method, [ '^disable', '^cron', '^yum_cron'])
+
+    # define patch_method settings
+    case $method {
+        'yum_cron': {
+            # settings for yum_cron
+            $patch_script_ensure = 'absent'
+            $cron_entry_ensure = 'absent'
+            $yum_cron_ensure = 'present'
+            $yum_cron_enable = true
+        }
+        'cron': {
+            # settings for cron
+            $patch_script_ensure = 'file'
+            $cron_entry_ensure = 'present'
+            $yum_cron_ensure = 'absent'
+            $yum_cron_enable = false
+
+        }
+        'disable': {
+            # settings for disable
+            $patch_script_ensure = 'absent'
+            $cron_entry_ensure = 'absent'
+            $yum_cron_ensure = 'absent'
+            $yum_cron_enable = false
+        }
+        default: {
+            # disable it all
+            $patch_script_ensure = 'absent'
+            $cron_entry_ensure = 'absent'
+            $yum_cron_ensure = 'absent'
+            $yum_cron_enable = false
+        }
+    }
+
+    # patch script configuration
+    file { $cron_script_dst:
+        ensure  => $patch_script_ensure,
+        source  => $cron_script_src,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+    }
+
+    # patch script cron entry
+    cron { 'syswoner-patch-install':
+        ensure    => $cron_entry_ensure,
+        command   => "${cron_script_dst} reboot=${cron_reboot} 2>&1 >/dev/null",
+        user      => 'root',
+        minute    => $cron_minute,
+        hour      => $cron_hour,
+        monthday  => $cron_monthday,
+        month     => $cron_month,
+        weekday   => $cron_weekday,
+    }
+
+    # configure yum_cron
+    include yum_cron
+
+    # create the class
+    class { 'yum_cron':
+        ensure => $yum_cron_ensure,
+        enable => $yum_cron_enable,
+        mailto => $yum_cron_mailto,
+
+        apply_updates => $yum_cron_apply_updates,
+        download_updates => $yum_cron_download_updates,
+        days_of_week => $yum_cron_days_of_week,
+        update_cmd => $yum_cron_update_cmd,
+    }
+}
