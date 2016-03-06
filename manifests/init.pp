00a48293aac27af5ce4a4cdd8aaf91ac488efead
thechuckmo <chuck@moosejudge.com>
2016-03-06 12:14:36 -0800

    - Removing system_groups - Adding system_group

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,36 @@
 #
 # sysowner
 #
+# Parametets
+#
+# sysowner::system_owners:
+#  List of users and/or distribution lists of stake holders of the system.
+#  - Will includes owners from node, role, and group.
+#
+# sysowner::system_role:
+#  A single defined purpose for the system. Based on the primary application running on the server.
+#  - A role config will be included into the system configuration, including a list of system_owners
+#  - Define needed system configuration to perform the role (kernel, limits, sudo, etc)
+#  - examples: mysql-db, oracle-db, weblogic, nginx, apache
+#
+# sysowner::system_group:
+#  A single group responsible for the delivery of the business services of the system.
+#  - A group config will be included into the system configuration, primarily a list of system_owners
+#  - Define group configuration needed by group administrators (banner logins, custom sudo, etc)
+#  - examples: finance, webx, dw, clinics
+#
+# sysowner::system_note:
+#  Free form string description of system.
+#
 class sysowner (
     #
     # owners of the system
     #
-    $system_owners = ['Henry Winkler',], # array of owners
-    $system_groups = ['AD HW Fans',], # array of groups
+    $system_owners = ['winklerh'], # array of owners
+    #
+    # group/team owner of system
+    #
+    $system_group = ['hwadmins',], # string of group
     #
     # role: one per system - in hiera ex: "roles/%{::system_role}" => roles/webproxy.yaml
     #
@@ -27,7 +51,7 @@ class sysowner (
     validate_array($system_owners)
 
     # verify system_groups
-    validate_array($system_groups)
+    validate_string($system_group)
 
     # include patch control
     include sysowner::patch
